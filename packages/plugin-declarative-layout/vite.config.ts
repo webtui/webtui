64d63c060c06d91f844359c1750a8db1d733347e
IroncladDev <conner@ironclad.sh>
2026-06-24 10:36:26 -0500

    feat: split padding/flexbox utils into declarative layout plugin

---
+++ packages/plugin-declarative-layout/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: 'src/index.css',
+            output: {
+                assetFileNames: '[name].[ext]',
+            },
+        },
+    },
+})
